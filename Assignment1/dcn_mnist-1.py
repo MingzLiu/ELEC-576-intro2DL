1efd8ed0c65562e20c1d56f62a58af929645b80f
MIngzLiu <ml116@rice.edu>
2021-09-29 00:49:07 -0500

    feat(Assignment1): add Assignment1 task2
    almost done the HW

--- Assignment1/dcn_mnist-1.py
+++ Assignment1/dcn_mnist-1.py
@@ -1,18 +1,20 @@
 import os
 import time
 
-
 import tensorflow as tf
 
-if(tf.__version__.split('.')[0]=='2'):
+if (tf.__version__.split('.')[0] == '2'):
     import tensorflow.compat.v1 as tf
-    tf.disable_v2_behavior()    
+
+    tf.disable_v2_behavior()
 
 # Load MNIST dataset
 import input_data
+
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 sess = tf.InteractiveSession()
+
 
 def weight_variable(shape):
     '''
@@ -26,8 +28,9 @@ def weight_variable(shape):
     '''
 
     # IMPLEMENT YOUR WEIGHT_VARIABLE HERE
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
 
-    return W
 
 def bias_variable(shape):
     '''
@@ -38,8 +41,9 @@ def bias_variable(shape):
     '''
 
     # IMPLEMENT YOUR BIAS_VARIABLE HERE
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
 
-    return b
 
 def conv2d(x, W):
     '''
@@ -59,7 +63,8 @@ def conv2d(x, W):
 
     # IMPLEMENT YOUR CONV2D HERE
 
-    return h_conv
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
 
 def max_pool_2x2(x):
     '''
@@ -70,58 +75,72 @@ def max_pool_2x2(x):
 
     # IMPLEMENT YOUR MAX_POOL_2X2 HERE
 
-    return h_max
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
+
+
+def variable_summaries(var):
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
 
 def main():
     # Specify training parameters
-    result_dir = './results/' # directory where the results from the training are saved
-    max_step = 5500 # the maximum iterations. After max_step iterations, the training will stop no matter what
+    result_dir = './results/'  # directory where the results from the training are saved
+    max_step = 5500  # the maximum iterations. After max_step iterations, the training will stop no matter what
 
-    start_time = time.time() # start timing
+    start_time = time.time()  # start timing
 
     # FILL IN THE CODE BELOW TO BUILD YOUR NETWORK
 
     # placeholders for input data and input labeles
-    x =
-    y_ =
+    x = tf.placeholder(tf.float32, [None, 784], name='x')
+    y_ = tf.placeholder(tf.float32, [None, 10], name='y_')
 
     # reshape the input image
-    x_image = tf.reshape(x, [-1, 28, 28, 1]
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
 
     # first convolutional layer
-    W_conv1 =
-    b_conv1 =
-    h_conv1 =
-    h_pool1 =
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
 
     # second convolutional layer
-    W_conv2 =
-    b_conv2 =
-    h_conv2 =
-    h_pool2 =
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
 
     # densely connected layer
-    W_fc1 =
-    b_fc1 =
-    h_pool2_flat =
-    h_fc1 =
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     # dropout
-    keep_prob =
-    h_fc1_drop =
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
     # softmax
-    W_fc2 =
-    b_fc2 =
-    y_conv =
+    W_fc2 = weight_variable([1024, 10])
+    b_fc2 = bias_variable([10])
+    y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2, name='y_conv')
 
     # FILL IN THE FOLLOWING CODE TO SET UP THE TRAINING
 
     # setup training
-    cross_entropy =
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction =
-    accuracy =
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
 
     # Add a scalar summary for the snapshot loss.
     tf.summary.scalar(cross_entropy.op.name, cross_entropy)
@@ -142,13 +161,13 @@ def main():
 
     # run the training
     for i in range(max_step):
-        batch = mnist.train.next_batch(50) # make the data batch, which is used in the training iteration.
-                                            # the batch size is 50
-        if i%100 == 0:
+        batch = mnist.train.next_batch(50)  # make the data batch, which is used in the training iteration.
+        # the batch size is 50
+        if i % 100 == 0:
             # output the training accuracy every 100 iterations
             train_accuracy = accuracy.eval(feed_dict={
-                x:batch[0], y_:batch[1], keep_prob: 1.0})
-            print("step %d, training accuracy %g"%(i, train_accuracy))
+                x: batch[0], y_: batch[1], keep_prob: 1.0})
+            print("step %d, training accuracy %g" % (i, train_accuracy))
 
             # Update the events file which is used to monitor the training (in this case,
             # only the training loss is monitored)
@@ -161,15 +180,15 @@ def main():
             checkpoint_file = os.path.join(result_dir, 'checkpoint')
             saver.save(sess, checkpoint_file, global_step=i)
 
-        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5}) # run one train_step
+        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})  # run one train_step
 
     # print test error
-    print("test accuracy %g"%accuracy.eval(feed_dict={
+    print("test accuracy %g" % accuracy.eval(feed_dict={
         x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
     stop_time = time.time()
-    print('The training takes %f second to finish'%(stop_time - start_time))
+    print('The training takes %f second to finish' % (stop_time - start_time))
+
 
 if __name__ == "__main__":
     main()
-
