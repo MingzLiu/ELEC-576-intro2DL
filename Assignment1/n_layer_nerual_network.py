54a24699c354cd4ed44ef9e324f875b8a9f683b7
MingzLiu <ml116@rice.edu>
2021-09-29 13:00:36 -0500

    feat(Assignment1): Final HW1 commit

--- Assignment1/n_layer_nerual_network.py
+++ Assignment1/n_layer_nerual_network.py
@@ -1,4 +1,3 @@
-from Assignment1.three_layer_neural_network import NeuralNetwork
 import numpy as np
 from sklearn import datasets, linear_model
 import matplotlib.pyplot as plt
@@ -11,6 +10,12 @@ def generate_data():
     '''
     np.random.seed(0)
     X, y = datasets.make_moons(200, noise=0.20)
+    return X, y
+
+
+def generate_other_data():
+    np.random.seed(0)
+    X, y = datasets.make_circles(n_samples=100, shuffle=True, noise=None, random_state=None, factor=0.8)
     return X, y
 
 
@@ -37,9 +42,70 @@ def plot_decision_boundary(pred_func, X, y):
     plt.show()
 
 
-class DeepNeuralNetwork(NeuralNetwork):
-    def __init__(self, nn_input_dim, nn_hidden_dim, nn_output_dim):
-        super().__init__(nn_input_dim, nn_hidden_dim, nn_output_dim)
+class DeepNeuralNetwork(object):
+    """
+    This class builds and trains a deep neural network
+    """
+
+    def __init__(self, nn_dims, actFun_type='tanh', reg_lambda=0.01, seed=0):
+        '''
+        :param nn_dims: dimension of each layers
+        :param actFun_type: type of activation function. 3 options: 'tanh', 'sigmoid', 'relu'
+        :param reg_lambda: regularization coefficient
+        :param seed: random seed
+        '''
+        self.nn_dims = nn_dims
+        self.actFun_type = actFun_type
+        self.reg_lambda = reg_lambda
+
+        # initialize the weights and biases in the network
+        self.W = []
+        self.b = []
+
+        np.random.seed(seed)
+        for i in range(len(nn_dims) - 1):
+            self.W.append(np.random.randn(self.nn_dims[i], self.nn_dims[i + 1])
+                          / np.sqrt(self.nn_dims[i]))
+            self.b.append(np.zeros((1, self.nn_dims[i + 1])))
+
+    def actFun(self, z, type):
+        '''
+        actFun computes the activation functions
+        :param z: net input
+        :param type: Tanh, Sigmoid, or ReLU
+        :return: activations
+        '''
+
+        # YOU IMPLMENT YOUR actFun HERE
+
+        if type == "tanh":
+            return np.tanh(z)
+        elif type == "sigmoid":
+            return 1 / (1 + np.exp(-z))
+        elif type == "relu":
+            return np.maximum(0, z)
+        else:
+            return None
+
+    def diff_actFun(self, z, type):
+        '''
+        diff_actFun computes the derivatives of the activation functions wrt the net input
+        :param z: net input
+        :param type: Tanh, Sigmoid, or ReLU
+        :return: the derivatives of the activation functions wrt the net input
+        '''
+
+        # YOU IMPLEMENT YOUR diff_actFun HERE
+
+        if type == 'tanh':
+            return 1 - np.square(np.tanh(z))
+        elif type == 'sigmoid':
+            tmp = 1 / (1 + np.exp(-z))
+            return tmp * (1 - tmp)
+        elif type == 'relu':
+            return np.where(z > 0, 1, 0)
+        else:
+            return None
 
     def feedforward(self, X, actFun):
         '''
@@ -52,15 +118,22 @@ class DeepNeuralNetwork(NeuralNetwork):
 
         # YOU IMPLEMENT YOUR feedforward HERE
 
-        self.z1 = np.dot(X, self.W1) + self.b1
-        self.a1 = actFun(self.z1)
-        self.z2 = np.dot(self.a1, self.W2) + self.b2
-        self.probs = np.exp(self.z2) / np.sum(np.exp(self.z2), axis=1, keepdims=True)  # softmax(z)
+        self.z = []
+        self.a = []
+        for i in range(len(self.W)):
+            if i == 0:
+                self.z.append(np.dot(X, self.W[i]) + self.b[i])
+            else:
+                self.z.append(np.dot(self.a[i - 1], self.W[i]) + self.b[i])
+            if i != len(self.W) - 1:
+                self.a.append(actFun(self.z[i]))
+        exp_scores = np.exp(self.z[len(self.z) - 1])
+        self.probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
         return None
 
     def calculate_loss(self, X, y):
         '''
-        calculate_loss compute the loss for prediction
+        calculate_loss computes the loss for prediction
         :param X: input data
         :param y: given labels
         :return: the loss for prediction
@@ -70,10 +143,17 @@ class DeepNeuralNetwork(NeuralNetwork):
         # Calculating the loss
 
         # YOU IMPLEMENT YOUR CALCULATION OF THE LOSS HERE
-        data_loss = np.sum(-np.log(self.probs[range(num_examples), y]))
+
+        probs = np.exp(self.z[len(self.z) - 1]) / \
+                np.sum(np.exp(self.z[len(self.z) - 1]), axis=1, keepdims=True)
+        data_loss_single = -np.log(probs[range(num_examples), y])
+        data_loss = np.sum(data_loss_single)
 
         # Add regulatization term to loss (optional)
-        data_loss += self.reg_lambda / 2 * (np.sum(np.square(self.W1)) + np.sum(np.square(self.W2)))
+        W_sum = 0
+        for i in len(self.W):
+            W_sum += np.sum(np.square(self.W[i]))
+        data_loss += self.reg_lambda / 2 * W_sum
         return (1. / num_examples) * data_loss
 
     def predict(self, X):
@@ -87,26 +167,31 @@ class DeepNeuralNetwork(NeuralNetwork):
 
     def backprop(self, X, y):
         '''
-        backprop run backpropagation to compute the gradients used to update the parameters in the backward step
+        backprop implements backpropagation to compute the gradients used to update the parameters in the backward step
         :param X: input data
         :param y: given labels
-        :return: dL/dW1, dL/b1, dL/dW2, dL/db2
+        :return: dL/dW1, dL/b1, dL/dW2, dL/db2, ... dL/dn, dL/bn in two lists
         '''
 
         # IMPLEMENT YOUR BACKPROP HERE
         num_examples = len(X)
-        tmp = self.probs
-        tmp[range(num_examples), y] -= 1
-        # dW2 = dL/dW2
-        dW2 = np.dot(self.a1.T, tmp)
-        # db2 = dL/db2
-        db2 = np.sum(tmp, axis=0, keepdims=True)
-        # dW1 = dL/dW1
-        tmp2 = np.dot(tmp, self.W2.T) * self.diff_actFun(self.z1, type=self.actFun_type)
-        dW1 = np.dot(X.T, tmp2)
-        # db1 = dL/db1
-        db1 = np.sum(tmp2, axis=0, keepdims=False)
-        return dW1, dW2, db1, db2
+        delta = self.probs
+        delta[range(num_examples), y] -= 1
+
+        dW = []
+        db = []
+        for i in range(len(self.z)):
+            index = len(self.z) - i - 1
+            if index != 0:
+                dW.insert(0, np.dot(self.a[index - 1].T, delta))
+                db.insert(0, np.sum(delta, axis=0, keepdims=True))
+                delta = np.dot(delta, self.W[index].T) * \
+                        self.diff_actFun(self.z[index - 1], type=self.actFun_type)
+            else:
+                dW.insert(0, np.dot(X.T, delta))
+                db.insert(0, np.sum(delta, axis=0, keepdims=False))
+
+        return dW, db
 
     def fit_model(self, X, y, epsilon=0.01, num_passes=20000, print_loss=True):
         '''
@@ -122,31 +207,43 @@ class DeepNeuralNetwork(NeuralNetwork):
             # Forward propagation
             self.feedforward(X, lambda x: self.actFun(x, type=self.actFun_type))
             # Backpropagation
-            dW1, dW2, db1, db2 = self.backprop(X, y)
+            dW, db = self.backprop(X, y)
 
-            # Add derivatives of regularization terms (b1 and b2 don't have regularization terms)
-            dW2 += self.reg_lambda * self.W2
-            dW1 += self.reg_lambda * self.W1
+            # Add regularization terms (b1 and b2 don't have regularization terms)
+            for i in range(len(dW)):
+                # print(dW[i].shape)
+                # print(self.W[i].shape)
+                dW[i] += self.reg_lambda * self.W[i]
 
             # Gradient descent parameter update
-            self.W1 += -epsilon * dW1
-            self.b1 += -epsilon * db1
-            self.W2 += -epsilon * dW2
-            self.b2 += -epsilon * db2
+            for i in range(len(self.W)):
+                self.W[i] += -epsilon * dW[i]
+                self.b[i] += -epsilon * db[i]
 
             # Optionally print the loss.
             # This is expensive because it uses the whole dataset, so we don't want to do it too often.
             if print_loss and i % 1000 == 0:
                 print("Loss after iteration %i: %f" % (i, self.calculate_loss(X, y)))
 
+    def visualize_decision_boundary(self, X, y):
+        '''
+        visualize_decision_boundary plots the decision boundary created by the trained network
+        :param X: input data
+        :param y: given labels
+        :return:
+        '''
+        plot_decision_boundary(lambda x: self.predict(x), X, y)
+
 
 def main():
-    # generate and visualize Make-Moons dataset
-    X, y = generate_data()
+    # # generate and visualize Make-Moons dataset
+    # X, y = generate_data()
+    X, y = generate_other_data()
     # plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
     # plt.show()
 
-    model = NeuralNetwork(nn_input_dim=2, nn_hidden_dim=3, nn_output_dim=2, actFun_type='tanh')
+    # model = NeuralNetwork(nn_input_dim=2, nn_hidden_dim=3 , nn_output_dim=2, actFun_type='tanh')
+    model = DeepNeuralNetwork(nn_dims=[2, 3, 3, 2], actFun_type='tanh')
     model.fit_model(X, y)
     model.visualize_decision_boundary(X, y)
 
